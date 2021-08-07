4c5b7396123db63524527d663c1505916dadcd05
adam-wills <81123416+adam-wills@users.noreply.github.com>
2021-08-07 14:43:27 -0700

    Update MIDItoPhaseIncrement.sv

--- MIDItoPhaseIncrement.sv
+++ MIDItoPhaseIncrement.sv
@@ -4,26 +4,25 @@ module MIDItoPhaseIncrement
 )
 (
       input  logic        clk,
-		input  logic [6:0]  midi_notenums[0:NUMREADS-1],
+      input  logic [6:0]  midi_notenums[0:NUMREADS-1],
       output logic [31:0] phase_increments[0:NUMREADS-1]
 );
 
    reg [31:0] data_reg[0:NUMREADS-1];
    reg [31:0] indexToPhaseIncrement[0:127]
 
-	initial begin
-		readmemh("./inferredMem/midiNNtoPI.txt", indexToPhaseIncrement);
-	end
+   initial begin
+      readmemh("./inferredMem/midiNNtoPI.txt", indexToPhaseIncrement);
+   end
 	
    genvar i;
-	generate
-		for (i = 0; i < NUMREADS; i = i+1) begin
-			always_ff @ (posedge clk)
-					data_reg[i] <= indexToPhaseIncrement[midi_notenums[i]];
+      generate
+         for (i = 0; i < NUMREADS; i = i+1) begin
+            always_ff @ (posedge clk)
+               data_reg[i] <= indexToPhaseIncrement[midi_notenums[i]];
 			
-			assign phase_increments[i] = data_reg[i];
-		end
-	endgenerate
-	
+             assign phase_increments[i] = data_reg[i];
+            end
+      endgenerate
 	
 endmodule
